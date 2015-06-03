7d4e9a41fa32cfdbd67428a58d2453b770215f3f
mrrrgn <winter2718@gmail.com>
2015-06-03 07:06:00 -0800

    https://bugzilla.mozilla.org/attachment.cgi?id=8614672&action=edit

--- start-runner.bat
+++ start-runner.bat
@@ -1,5 +1,3 @@
-@echo off
-
 SET MOZBUILDDIR=C:\mozilla-build
 SET MOZILLABUILD=%MOZBUILDDIR%
 echo "MozillaBuild directory: %MOZBUILDDIR%"
@@ -7,8 +5,6 @@ echo "MozillaBuild directory: %MOZBUILDDIR%"
 REM set BUILDBOT_PATH, the path to the active Buildbot virtualenv
 call "C:/mozilla-build/bbpath.bat"
 echo "Buildbot virtualenv: %BUILDBOT_PATH%"
-
-set log="c:\tmp\buildbot-startup.log"
 
 REM Use the "new" moztools-static
 set MOZ_TOOLS=%MOZBUILDDIR%\moztools-x64
@@ -18,9 +14,4 @@ SET PATH=%PATH%;%MOZ_TOOLS%\bin
 
 cd "%USERPROFILE%"
 
-:start
-echo "%date% %time% - About to run runner.py"
-
-%BUILDBOT_PATH%\Scripts\runner -n 5 -H -c c:\opt\runner\runner.cfg c:\opt\runner\tasks.d
-
-echo "%date% %time% - runner.py finished"
+%BUILDBOT_PATH%\Scripts\runner -n 5 -H -c c:\opt\runner\runner.cfg c:\opt\runner\tasks.d > c:\tmp\runner.log
